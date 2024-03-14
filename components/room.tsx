6bfaa64069d319f518760c3b53ee1558183062c0
Huy Nguyen <quanghuy.nguyennhu@gmail.com>
2024-03-14 15:18:53 +0200

    add liveblocks

---
+++ components/room.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { ReactNode } from "react";
+import { ClientSideSuspense } from "@liveblocks/react";
+
+import { RoomProvider } from "@/liveblocks.config";
+
+interface RoomProps { 
+  children: ReactNode
+  roomId: string;
+  fallback: NonNullable<ReactNode> | null;
+};
+
+export const Room = ({ 
+  children,
+  roomId,
+  fallback,
+}: RoomProps) => {
+  return (
+    <RoomProvider 
+      id={roomId} 
+      initialPresence={{
+        cursor: null,
+        selection: [],
+        pencilDraft: null,
+        penColor: null,
+      }}
+    >
+      <ClientSideSuspense fallback={fallback}>
+        {() => children}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
