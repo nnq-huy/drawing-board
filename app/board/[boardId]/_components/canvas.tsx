60499aba4611b100da60995d1da94f68c05e4f99
Huy Nguyen <quanghuy.nguyennhu@gmail.com>
2024-03-14 16:04:08 +0200

    canvas info bar, toolbar, users bar

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,8 +1,23 @@
 "use client";
 
+import {useState} from "react";
+
+import { 
+  useHistory, 
+  useCanUndo, 
+  useCanRedo,
+} from "@/liveblocks.config";
+
+import { 
+  CanvasMode, 
+  CanvasState, 
+
+} from "@/types/canvas";
+
 import { Info } from "./info";
-import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
+import { Participants } from "./participants";
+
 
 interface CanvasProps {
   boardId: string;
@@ -12,11 +27,33 @@ export const Canvas = ({
   boardId,
 }: CanvasProps) => {
 
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+ 
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+
+
+
+
   return (
-    <main className="h-full w-full relative bg-neutral-100 touch-none">
-      <Info/>
-      <Participants/>
-      <Toolbar/>
+    <main
+      className="h-full w-full relative bg-neutral-100 touch-none"
+    >
+      <Info boardId={boardId} />
+      <Participants />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   );
 };
