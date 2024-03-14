1c992901d1b3ebe51ca6f5309602d2437ae37f45
Huy Nguyen <quanghuy.nguyennhu@gmail.com>
2024-03-14 14:09:06 +0200

    board screen base setup

---
+++ app/board/[boardId]/_components/participants.tsx
@@ -0,0 +1,13 @@
+export const Participants = () => {
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md ">
+      active users list
+    </div>
+  )
+}
+
+export const ParticipantsSkeleton = () => {
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]" />
+  );
+};
